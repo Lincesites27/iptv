b13c06b1609a8ef8970840e3afff85397fd69143
Lincesites27 <contato@lincesites.com.br>
2023-05-02 22:12:51 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Section1 from './Sections/section1/Section1';
 import Section2 from './Sections/section2/Section2';
 import Section3 from './Sections/section3/Section3';
+import Section4 from './Sections/section4/Section4';
+import Section5 from './Sections/section5/Section5';
 
 
 
@@ -13,9 +15,9 @@ function App() {
       <Section1 />
       <Section2 />
       <Section3 />
-     
-   
-     
+      <Section4 />
+      <Section5 />
+                   
     </div>
   );
 }
